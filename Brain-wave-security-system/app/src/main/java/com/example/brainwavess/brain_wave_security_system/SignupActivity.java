9f7c20211f78e450ef2466e92ab894029c9d43df
misinayu <yama_masa22@yahoo.co.jp>
2017-05-22 08:51:36 +0900

    Default UI

---
+++ Brain-wave-security-system/app/src/main/java/com/example/brainwavess/brain_wave_security_system/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.brainwavess.brain_wave_security_system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
